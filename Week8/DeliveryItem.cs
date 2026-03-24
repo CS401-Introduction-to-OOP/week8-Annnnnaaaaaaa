3583ce7521b6aabb7da02199dea175af24f685c6
Annnnnaaaaaaa <dnipro.anna@gmail.com>
2026-03-24 18:32:55 +0200

    DeliveryItem class

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get ; }
+    public double Weight { get; }
+    
+    public DeliveryItem (string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost()
+    {
+        return deviveryCost; // повертає вартість доставки
+    }
+
+    public virtual string PrintInfo()
+    {
+        return $"Tracking number: {trackingNumber}, weight: {weight}";
+    }
+}
